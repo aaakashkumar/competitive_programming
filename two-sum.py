d0a7e39ba9495cbb7100f7c1683fa750a0190d79
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2020-11-01 18:13:46 +0000

    Use One Hash method

--- two-sum.py
+++ two-sum.py
@@ -12,29 +12,21 @@ class Solution:
         list that can be summed to obtain target
         """
 
-        nums = sorted(enumerate(nums), key=lambda x: x[1])
-
-        left = 0
-        right = len(nums) - 1
-
-        while left < right:
-            two_sum = nums[left][1] + nums[right][1]
-            if two_sum == target:
-                return [nums[left][0], nums[right][0]]
-
-            elif two_sum > target:
-                right -= 1
-
-            elif two_sum < target:
-                left += 1
+        hash = {}
+        for index, element in enumerate(nums):
+            complement = target - element
+            if complement in hash:
+                return [index, hash[complement]]
+            else:
+                hash[element] = index
     
     def test_twoSum(self):
         """
         Method to test our twoSum solution
         """
-        assert self.twoSum([2,7,11,15], 9) == [0, 1]
-        assert self.twoSum([3,2,4], 6) == [1, 2]
-        assert self.twoSum([3,3], 6) == [0, 1]
+        assert set(self.twoSum([2,7,11,15], 9)) == set([0, 1])
+        assert set(self.twoSum([3,2,4], 6)) == set([1, 2])
+        assert set(self.twoSum([3,3], 6)) == set([0, 1])
         print("All sample cases passed")
 
 Solution().test_twoSum()
