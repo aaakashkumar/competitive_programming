72c7b28cb8129a7c5a0a80f74ce0f0c019e41691
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2020-11-05 07:07:09 +0000

    Add more test cases; modify solution

--- 4sum.py
+++ 4sum.py
@@ -11,32 +11,35 @@ class Solution:
         """
 
         quadruplets = list()
+        nums = sorted(nums)
 
         for i in range(len(nums)-3):
             for j in range(i+1, len(nums)-2):
+
                 hash_ = []
                 for k in range(j+1, len(nums)):
-                    # print(i,j,k)
+                    complement = target-(nums[i]+nums[j]+nums[k])
 
                     # check if the complement (the last number that should be added)
                     # has already been found (in the hash)
-                    complement = target-(nums[i]+nums[j]+nums[k])
                     if complement in hash_:
                         # print(i,j,k,complement)
-
-                        # sort the numbers so that they can be searched the next time to avoid duplicates
-                        temp_quad = sorted([nums[i], nums[j], nums[k], complement])
-                        if temp_quad not in quadruplets:
-                            quadruplets.append(temp_quad)
+                        if [nums[i], nums[j], nums[k], complement] not in quadruplets:
+                            quadruplets.append([nums[i], nums[j], nums[k], complement])
                         
                     hash_.append(nums[k])
         
         return quadruplets
 
     def test_fourSum(self):
-        assert sorted(self.fourSum([1,0,-1,0,-2,2], 0)) == sorted([[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]])
+        def inner_sort(arr):
+            return sorted([sorted(x) for x in arr])
+
+        assert inner_sort(self.fourSum([1,0,-1,0,-2,2], 0)) == inner_sort([[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]])
         assert self.fourSum([], 0) == []
-        assert sorted(self.fourSum([-2,-1,-1,1,1,2,2], 0)) == sorted([[-2,-1,1,2],[-1,-1,1,1]])
+        assert inner_sort(self.fourSum([-2,-1,-1,1,1,2,2], 0)) == inner_sort([[-2,-1,1,2],[-1,-1,1,1]])
+        assert self.fourSum([0,0,0,0], 0) == [[0,0,0,0]]
+        assert inner_sort(self.fourSum([-5,5,4,-3,0,0,4,-2], 4)) == inner_sort([[-5,0,4,5],[-3,-2,4,5]])
         print("All test cases ran successfully")
 
 Solution().test_fourSum()
