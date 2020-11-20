f69e3eb427baa0ae53bec0550ecd5a3be6acc538
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2020-11-20 08:42:24 +0000

    Working solution

--- permutation-sequence.py
+++ permutation-sequence.py
@@ -7,9 +7,13 @@ class Solution:
 
         for i in range(1,num+1):
             self.factorial.append(self.factorial[i-1] * i)
+        
+        return self.factorial
 
     def getPermutation(self, n: int, k: int) -> str:
-        self.calculateFactorial(9)
+        # hardcode self.factorial = [1,1,2,6,24,120,720,5040,40320,362880] for faster performance
+        self.calculateFactorial(9)  
+        
         nums = list(range(1,n+1))
         
         index = 0
@@ -18,9 +22,13 @@ class Solution:
         while k != 0:
             possible_numbers = self.factorial[n-(index+1)]
 
-            if possible_numbers <= k:
+            if possible_numbers == k:
                 k -= possible_numbers
-                if k == 0: break
+                nums[index+1:] = reversed(nums[index+1:])
+                break
+
+            if possible_numbers < k:
+                k -= possible_numbers
                 nums[index], nums[swap_index] = nums[swap_index], nums[index]
                 swap_index += 1
             
@@ -34,6 +42,7 @@ class Solution:
         assert self.getPermutation(3,3) == "213"
         assert self.getPermutation(4,9) == "2314"
         assert self.getPermutation(3,1) == "123"
+        assert self.getPermutation(3,2) == "132"
         
         print("All test cases ran successfully")
 
