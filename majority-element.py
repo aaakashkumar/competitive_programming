ed07dc15bd2af195cf343ebd63b42dd467ec5465
aaakashkumar <piratedvirus@gmail.com>
2020-11-24 06:02:49 +0000

    Add Boyer-Moore Voting Algorithm

--- majority-element.py
+++ majority-element.py
@@ -22,6 +22,26 @@ class Solution:
             if num_count[i] >= nums_length / 2:
                 return i
 
+    def majorityElementBoyerMoore(self, nums: List[int]) -> int:
+        """
+        Method to calculate the majority element using Boyer-Moore Voting Algorithm
+        :param nums: a list of integers
+        """
+        current_majority = None
+        majority_count = 0
+        majority_target = len(nums)//2
+
+        for i in nums:
+            if majority_count == 0:
+                current_majority = i
+
+            if i == current_majority:
+                majority_count += 1
+            else:
+                majority_count -= 1
+        
+        return current_majority
+
     def testMajorityElement(self):
         """
         A method to test majorityElement()
@@ -29,6 +49,7 @@ class Solution:
         assert self.majorityElement([3,2,3]) == 3
         assert self.majorityElement([2,2,1,1,1,2,2]) == 2
         assert self.majorityElement([1]) == 1
+        assert self.majorityElement([6,5,5]) == 5
 
         print("All test cases ran successfully")
 
