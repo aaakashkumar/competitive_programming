ef49cfe36147b71ef3422e9b1b4fc2403f1edb4e
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2020-12-05 08:12:27 +0000

    Add a test method for testing sample cases

--- codechef/VACCINE1.py
+++ codechef/VACCINE1.py
@@ -29,6 +29,18 @@ def get_days_required(d1, v1, d2, v2, p):
         return days_required + math.ceil(p/(v1+v2))
 
 
+def test_get_days_required():
+
+    assert get_days_required(1, 2, 1, 3, 14) == 3
+    assert get_days_required(5, 4, 2, 10, 100) == 9
+    assert get_days_required(5, 4, 3, 2, 1) == 3
+
+    print("All sample test cases ran successfully")
+
+
+# # uncomment this line and comment the  input lines for testing
+# test_get_days_required()
+
 d1, v1, d2, v2, p = map(int, input().split())
 print(get_days_required(d1, v1, d2, v2, p))
 
