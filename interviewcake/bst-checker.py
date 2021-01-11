2b482ee41b05fa2860af9bf4eb3ee1005a75e085
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2021-01-11 19:21:01 +0000

    Add Binary Search Tree Checker
    (PS. I know I can avoid those temp_variables!)

---
+++ interviewcake/bst-checker.py
@@ -0,0 +1,108 @@
+import unittest
+
+
+def is_binary_search_tree(root):
+
+    # Determine if the tree is a valid binary search tree
+    nodes = [(root, -float('inf'), float('inf'))]  # node, lower_bound, upper_bound
+
+    while nodes:
+        node, lower_bound, upper_bound = nodes.pop()
+
+        if node.value > upper_bound or node.value < lower_bound:
+            return False 
+
+        if node.left:
+            temp_upper_bound = upper_bound
+            if node.value < upper_bound: temp_upper_bound = node.value
+            nodes.append((node.left, lower_bound, temp_upper_bound))
+        
+        if node.right:
+            temp_lower_bound = lower_bound
+            if node.value > lower_bound: temp_lower_bound = node.value
+            nodes.append((node.right, temp_lower_bound, upper_bound))
+
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    class BinaryTreeNode(object):
+
+        def __init__(self, value):
+            self.value = value
+            self.left = None
+            self.right = None
+
+        def insert_left(self, value):
+            self.left = Test.BinaryTreeNode(value)
+            return self.left
+
+        def insert_right(self, value):
+            self.right = Test.BinaryTreeNode(value)
+            return self.right
+
+    def test_valid_full_tree(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(70)
+        left.insert_left(10)
+        left.insert_right(40)
+        right.insert_left(60)
+        right.insert_right(80)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+    def test_both_subtrees_valid(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(30)
+        right = tree.insert_right(80)
+        left.insert_left(20)
+        left.insert_right(60)
+        right.insert_left(70)
+        right.insert_right(90)
+        result = is_binary_search_tree(tree)
+        self.assertFalse(result)
+
+    def test_descending_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        left = tree.insert_left(40)
+        left_left = left.insert_left(30)
+        left_left_left = left_left.insert_left(20)
+        left_left_left.insert_left(10)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+    def test_out_of_order_linked_list(self):
+        tree = Test.BinaryTreeNode(50)
+        right = tree.insert_right(70)
+        right_right = right.insert_right(60)
+        right_right.insert_right(80)
+        result = is_binary_search_tree(tree)
+        self.assertFalse(result)
+
+    def test_one_node_tree(self):
+        tree = Test.BinaryTreeNode(50)
+        result = is_binary_search_tree(tree)
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
