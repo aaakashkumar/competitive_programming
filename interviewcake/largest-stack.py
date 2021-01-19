33f7fef7c24b1298b2cfaa68d21198f804f90bf1
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2021-01-19 17:22:35 +0000

    Find largest value in Stack in O(1) time and O(1) additional space
    Solved the bonus problem given in the Largest Stack problem (https://www.interviewcake.com/question/python3/largest-stack?course=fc1&section=queues-stacks).
    
    MaxStackBonus() uses constant time and constant space. Also added explanation as comments.

--- interviewcake/largest-stack.py
+++ interviewcake/largest-stack.py
@@ -70,6 +70,63 @@ class MaxStack(object):
         return self.max_stack.peek()
 
 
+class MaxStackBonus(object):
+    """
+    This class implements solution to the bonus problem, i.e., to 
+    implement the MaxStack in O(1) time and O(1) additional space
+
+    Logic:
+    The logic is similar to what's given here: 
+    https://www.geeksforgeeks.org/find-maximum-in-a-stack-in-o1-time-and-o1-extra-space/
+
+    However, no need to do a complicated (2*x – maxEle) as described
+    
+    The following simple equations are way easier to understand:
+    1. top = previous max + current max
+        when pushing into the stack, update top as the above in case a new max is found
+
+    2. previous max = top - current max
+        while popping, just do the above to get the previous maximum value
+    """
+
+
+    def __init__(self):
+        self.stack = Stack()
+        self.maxItem = None
+
+    def push(self, item):
+        if self.stack.peek() is None:
+            self.stack.push(item)
+            self.maxItem = item
+            
+        else:
+            if item > self.maxItem:
+                # top = previous max + current max
+                self.stack.push(item + self.maxItem)
+                self.maxItem = item
+                
+            else:
+                self.stack.push(item)
+
+    def pop(self):
+        if self.stack.peek() is None:
+            return None
+            
+        if self.stack.peek() > self.maxItem:
+            temp = self.maxItem  # the value to be returned
+
+            # previous max = top - current max
+            self.maxItem = self.stack.peek() - self.maxItem
+
+            self.stack.pop()
+            return temp
+            
+        else:
+            return self.stack.pop()
+
+    def get_max(self):
+        return self.maxItem
+
 
 
 
@@ -92,6 +149,56 @@ class Test(unittest.TestCase):
 
     def test_stack_usage(self):
         max_stack = MaxStack()
+
+        max_stack.push(5)
+
+        actual = max_stack.get_max()
+        expected = 5
+        self.assertEqual(actual, expected)
+
+        max_stack.push(4)
+        max_stack.push(7)
+        max_stack.push(7)
+        max_stack.push(8)
+
+        actual = max_stack.get_max()
+        expected = 8
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.pop()
+        expected = 8
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.get_max()
+        expected = 7
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.pop()
+        expected = 7
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.get_max()
+        expected = 7
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.pop()
+        expected = 7
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.get_max()
+        expected = 5
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.pop()
+        expected = 4
+        self.assertEqual(actual, expected)
+
+        actual = max_stack.get_max()
+        expected = 5
+        self.assertEqual(actual, expected)
+
+    def test_bonus_stack_usage(self):
+        max_stack = MaxStackBonus()
 
         max_stack.push(5)
 
