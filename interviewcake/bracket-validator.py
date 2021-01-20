c0fd44518f58d2d3cc6bc739bdd37ce989b55dba
Akash Kumar <32223788+aaakashkumar@users.noreply.github.com>
2021-01-20 13:54:18 +0000

    Add code to validation brackets

---
+++ interviewcake/bracket-validator.py
@@ -0,0 +1,88 @@
+import unittest
+
+
+def is_valid(code):
+    """
+    Determine if the input code is valid
+    """
+    
+    parenthesis_stack = []
+    
+    if code == "":
+        return True
+    
+    opening_bracket_map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    
+    for character in code:
+        
+        if character in opening_bracket_map:
+            if len(parenthesis_stack) == 0:
+                return False
+            if opening_bracket_map[character] == parenthesis_stack[-1]:
+                parenthesis_stack.pop()
+            else:
+                return False
+            
+        else:
+            parenthesis_stack.append(character)
+    
+    if len(parenthesis_stack) > 0:
+        return False
+    else:
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_valid_short_code(self):
+        result = is_valid('()')
+        self.assertTrue(result)
+
+    def test_valid_longer_code(self):
+        result = is_valid('([]{[]})[]{{}()}')
+        self.assertTrue(result)
+
+    def test_interleaved_openers_and_closers(self):
+        result = is_valid('([)]')
+        self.assertFalse(result)
+
+    def test_mismatched_opener_and_closer(self):
+        result = is_valid('([][]}')
+        self.assertFalse(result)
+
+    def test_missing_closer(self):
+        result = is_valid('[[]()')
+        self.assertFalse(result)
+
+    def test_extra_closer(self):
+        result = is_valid('[[]]())')
+        self.assertFalse(result)
+
+    def test_empty_string(self):
+        result = is_valid('')
+        self.assertTrue(result)
+
+
+unittest.main(verbosity=2)
